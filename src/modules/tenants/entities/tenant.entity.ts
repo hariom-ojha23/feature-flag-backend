9f0527e1f6405728c5b610ef8413d0a32947e2d2
hariom-ojha23 <hariomojha2301@gmail.com>
2026-07-16 23:11:43 +0530

    feat: enum for tenant status

--- src/modules/tenants/entities/tenant.entity.ts
+++ src/modules/tenants/entities/tenant.entity.ts
@@ -1,11 +1,7 @@
 import { Column, Entity, OneToMany } from 'typeorm'
 import { BaseEntity } from '../../../common/entities/base.entity'
 import { User } from '../../users/entities/user.entity'
-
-export enum TenantStatus {
-  ACTIVE = 'active',
-  SUSPENDED = 'suspended',
-}
+import { TenantStatus } from '../../../common/enums/tenant.enum'
 
 @Entity()
 export class Tenant extends BaseEntity {
