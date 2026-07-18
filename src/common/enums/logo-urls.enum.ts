76bdc0698c4d12851ff1c8342c69800905acef5b
hariom-ojha23 <hariomojha2301@gmail.com>
2026-07-18 06:54:42 +0530

    feat: added logo url in otp-verify-email template

---
+++ src/common/enums/logo-urls.enum.ts
@@ -0,0 +1,4 @@
+export enum LOGO_URLS {
+  LOGO = 'https://res.cloudinary.com/harry23/image/upload/v1784301440/1784235400050-removebg-preview_s4pb7u.png',
+  LOGO_WITH_NAME = ''
+}
--- src/modules/mail/services/mail-template.service.ts
+++ src/modules/mail/services/mail-template.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import { readFileSync } from 'fs'
 import { join } from 'path'
+import { LOGO_URLS } from '../../../common/enums/logo-urls.enum'
 
 @Injectable()
 export class MailTemplateService implements OnModuleInit {
   private templates = new Map<string, string>()
+  private logoUrl = LOGO_URLS.LOGO
 
   onModuleInit() {
     this.templates.set(
@@ -14,6 +16,10 @@ export class MailTemplateService implements OnModuleInit {
   }
 
   render(templateName: string, vars: Record<string, string>): string {
+    vars = {
+      ...vars,
+      LOGO_URL: this.logoUrl,
+    }
     let html = this.templates.get(templateName)
     if (!html) throw new Error(`Template "${templateName}" not found`)
 
